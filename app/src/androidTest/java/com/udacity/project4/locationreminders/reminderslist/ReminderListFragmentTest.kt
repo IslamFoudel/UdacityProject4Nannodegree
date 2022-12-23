9f3db3bc116c89f42c863f136c6aad8e326cdb50
IslamNabil <eslamfoudel@yahoo.com>
2022-12-23 08:37:29 +0200

    in this commit we test the navigation, and then data that is displaying on the UI,and error messages.

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,5 +1,6 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.app.Application
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
@@ -8,24 +9,95 @@ import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext.get
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.koin.test.get
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest {
+    private lateinit var context: Application
+    private lateinit var reminderData: ReminderDataSource
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+    @Before
+    fun initRepository() {
+        stopKoin()
+        context = getApplicationContext()
+        val module = module {
+            viewModel {
+                RemindersListViewModel(
+                    context,
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    context,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(context) }
+        }
+
+        startKoin {
+            modules(listOf(module))
+        }
+
+        reminderData = get()
+
+        runBlocking {
+            reminderData.deleteAllReminders()
+        }
+    }
+
+    @Test
+    fun remiderListDisplayedUI() {
+        val reminderUI = ReminderDTO(
+            "Reminder",
+            "Description",
+            "Location",
+            1.0,
+            2.0
+        )
+        runBlocking {
+            reminderData.saveReminder(reminderUI)
+        }
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        onView(withText(reminderUI.title)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(reminderUI.description)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(reminderUI.location)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun reminderListNavigateAddReminder() {
+        val action = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        action.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
 }
